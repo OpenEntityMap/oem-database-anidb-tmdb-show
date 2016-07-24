419de1eb872f4348b5293d6b554b351d10210ce2
Dean Gardiner <me@dgardiner.net>
2016-07-24 15:12:30 +1200

    Updated version in [setup.py]

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PACKAGE_NAME = 'oem-database-anidb-tmdb-show'
 def build_config():
     config = {
         'name': PACKAGE_NAME,
-        'version': '1.16.4',
+        'version': '1.16.0',
 
         'author': 'Dean Gardiner',
         'author_email': 'me@dgardiner.net',
